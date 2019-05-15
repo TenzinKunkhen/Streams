67d389df0060c03c961b7f07fd2981f01e246091
kunkhen <tenzin.kunkhen@ryerson.ca>
2019-05-15 12:27:07 -0400

    history changes

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,9 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {SIGN_IN
     ,SIGN_OUT,  CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM
    , DELETE_STREAM
     ,EDIT_STREAM } from './types';
-
 
 export const signIn = (userId) => {
     return {
@@ -18,10 +18,14 @@ export const signOut = () => {
     };
 };
 
-export const createStream = formValues =>  async dispatch => {
-        const res = await streams.post('/streams', formValues);
+export const createStream = formValues =>  async (dispatch, getState) => {
+    const {userId} = getState().auth;    
+    const res = await streams.post('/streams', {...formValues, userId});
 
         dispatch({type: CREATE_STREAM, payload: res.data});
+        // do some programmatic navigation to 
+        // get the user back to root (list of streams)
+        history.push('/');
  };
 
  export const fetchStreams = () => async dispatch => {
